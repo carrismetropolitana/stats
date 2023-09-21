fd4d35b978a357081c19b9ef321e85ba9e6c0c60
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-09-21 11:29:23 +0100

    Update table settings

--- feedback-collector/index.js
+++ feedback-collector/index.js
@@ -29,7 +29,6 @@ fastify.post('/feedback/stopsExplorerRealtime', async (request, reply) => {
     table: tableName,
     values: [
       {
-        timestamp: Date.now(),
         stop_id: request.body.stop_id,
         trip_id: request.body.trip_id,
         vehicle_id: request.body.vehicle_id,
@@ -38,6 +37,10 @@ fastify.post('/feedback/stopsExplorerRealtime', async (request, reply) => {
       },
     ],
     format: 'JSONEachRow',
+    clickhouse_settings: {
+      async_insert: 1,
+      wait_for_async_insert: 1,
+    },
   });
   return reply.send(200);
 });
@@ -51,15 +54,16 @@ fastify.listen({ port: 5050, host: '0.0.0.0' }, async (err, address) => {
   await client.command({
     query: `
         CREATE TABLE IF NOT EXISTS ${tableName} (
-            timestamp DateTime64(3, 'Europe/Lisbon'),
+            timestamp DateTime64(3, 'Europe/Lisbon') DEFAULT now(),
             stop_id FixedString(6),
-            trip_id Nullable(String),
-            vehicle_id Nullable(String),
+            trip_id String DEFAULT "",
+            vehicle_id String DEFAULT "",
             sentiment Enum('positive' = 1, 'negative' = 0),
             details String
         )
         ENGINE MergeTree()
-        ORDER BY (timestamp)
+        PRIMARY KEY (timestamp, vehicle_id, sentiment)
+        ORDER BY (timestamp, sentiment, trip_id, vehicle_id, details)
     `,
   });
 });
