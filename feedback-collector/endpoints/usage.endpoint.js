7529ae20bddb496838286c6865450b00025d6663
João de Vasconcelos <johnyvasconcelos@icloud.com>
2023-10-08 01:08:54 +0100

    Test

--- feedback-collector/endpoints/usage.endpoint.js
+++ feedback-collector/endpoints/usage.endpoint.js
@@ -3,22 +3,81 @@ const CLICKHOUSE = require('../services/CLICKHOUSE');
 
 //
 module.exports.website = async (request, reply) => {
-  await CLICKHOUSE.client.insert({
-    table: 'usage_website',
-    values: [
-      {
-        stop_id: request.body.stop_id,
-        trip_id: request.body.trip_id,
-        vehicle_id: request.body.vehicle_id,
-        sentiment: request.body.sentiment,
-        details: JSON.stringify(request.body.details),
-      },
-    ],
-    format: 'JSONEachRow',
-    clickhouse_settings: {
-      async_insert: 1,
-      wait_for_async_insert: 1,
-    },
+  //
+  console.log({
+    app_version: pjson.version,
+    //
+    referer: document.referrer,
+    //
+    fingerprint: await getCurrentBrowserFingerPrint(),
+    //
+    ua: request.body.ua,
+    ua_browser_name: request.body.ua_browser_name,
+    ua_browser_version: request.body.ua_browser_version,
+    ua_engine_name: request.body.ua_engine_name,
+    ua_engine_version: request.body.ua_engine_version,
+    ua_os_name: request.body.ua_os_name,
+    ua_os_version: request.body.ua_os_version,
+    ua_device_model: request.body.ua_device_model,
+    ua_device_type: request.body.ua_device_type,
+    ua_device_vendor: request.body.ua_device_vendor,
+    ua_cpu_architecture: request.body.ua_cpu_architecture,
+    //
+    device_timezone: request.body.device_timezone,
+    device_screen_width: request.body.device_screen_width,
+    device_screen_height: request.body.device_screen_height,
+    device_screen_orientation: request.body.device_screen_orientation,
+    device_locale: request.body.device_locale,
+    //
+    ip_address: request.ip,
+    //
+    event_key: request.body.event_key,
+    event_properties: request.body.event_properties,
+    //
   });
+  //
+
+  //   await CLICKHOUSE.client.insert({
+  //     table: 'usage_website',
+  //     values: [
+  //       {
+  //         app_version: pjson.version,
+  //         //
+  //         referer: document.referrer,
+  //         //
+  //         fingerprint: await getCurrentBrowserFingerPrint(),
+  //         //
+  //         ua: request.body.ua,
+  //         ua_browser_name: request.body.ua_browser_name,
+  //         ua_browser_version: request.body.ua_browser_version,
+  //         ua_engine_name: request.body.ua_engine_name,
+  //         ua_engine_version: request.body.ua_engine_version,
+  //         ua_os_name: request.body.ua_os_name,
+  //         ua_os_version: request.body.ua_os_version,
+  //         ua_device_model: request.body.ua_device_model,
+  //         ua_device_type: request.body.ua_device_type,
+  //         ua_device_vendor: request.body.ua_device_vendor,
+  //         ua_cpu_architecture: request.body.ua_cpu_architecture,
+  //         //
+  //         device_timezone: request.body.device_timezone,
+  //         device_screen_width: request.body.device_screen_width,
+  //         device_screen_height: request.body.device_screen_height,
+  //         device_screen_orientation: request.body.device_screen_orientation,
+  //         device_locale: request.body.device_locale,
+  //         //
+  //         ip_address: request.ip,
+  //         //
+  //         event_key: request.body.event_key,
+  //         event_properties: request.body.event_properties,
+  //         //
+  //       },
+  //     ],
+  //     format: 'JSONEachRow',
+  //     clickhouse_settings: {
+  //       async_insert: 1,
+  //       wait_for_async_insert: 1,
+  //     },
+  //   });
+
   return reply.send(200);
 };
